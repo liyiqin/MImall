c683339b88562db4aa658e5b58f6b6feee2a6f8d
liyiqing <1421251960@qq.com>
2022-09-22 19:34:45 +0800

    项目目录结构设置

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,18 @@
 const { defineConfig } = require('@vue/cli-service')
+
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  devServer: {
+    host: 'localhost',
+    port: 8080,
+    proxy: {
+      '/api': {
+        target: 'https://www.imooc.com',
+        changeOrigin: true,
+        pathRewrite: {
+          '/api': ''
+        }
+      }
+    }
+  }
 })
